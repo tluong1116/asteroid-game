f5a046ae52427f2313a0301c6d548b8129c9c4ed
tluong1116 <tluong1116@gmail.com>
2025-12-15 17:07:05 -0800

    add method for player to move around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+#from circleshape import CircleShape
+from player import Player
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -8,16 +10,31 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # Initiate player
+    player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+
+    # Game Loop
     while True:
         log_state()
         # Check if user hit the close button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        # initiate black game screen
         screen.fill('black')
+        # Draw player
+        player.draw(screen)
+        # Update player
+        player.update(dt)   
+        # Flip display
         pygame.display.flip()
+        
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
+        
 
 
 if __name__ == "__main__":
